f466b2f6fb0cd14c1e6901013dce8dd0d1ef1c0e
Sabike_Raja <sabikeraja95@gmail.com>
2020-01-24 19:03:37 +0530

    mailvalid2

--- mailValid.js
+++ mailValid.js
@@ -1,2 +1,4 @@
-
-    console.log('res: ');
+const emailExistence=require('email-existence');
+emailExistence.check('mukeshkannan22051999@calibraint.com', async(error, response)=>{
+    console.log('res: '+response);
+});
